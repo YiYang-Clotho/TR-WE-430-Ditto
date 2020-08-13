d73d7c1203c69a8fe04715c54346d14bf9d64ceb
Zhihao Liang <s3798366@student.rmit.edu.au>
2020-08-14 01:01:01 +0800

    add content

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+  try {
     $("#headerDiv").load("../../template/header.html");
     $("#footerDiv").load("../../template/footer.html");
-  });
+  } catch (error) {}
+  // try {
+  //   $("#headerDiv").load("./template/header.html");
+  //   $("#footerDiv").load("./template/footer.html");
+  // } catch (error) {}
+});
+// /template/header.html
