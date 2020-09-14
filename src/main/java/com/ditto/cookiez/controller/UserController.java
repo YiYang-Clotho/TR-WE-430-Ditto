e72bd0fb1394cfa20573efadcf9bfb4dc5bd4415
Zhihao Liang <s3798366@student.rmit.edu.au>
2020-09-15 01:21:56 +0800

    feat(view): add animate

--- src/main/java/com/ditto/cookiez/controller/UserController.java
+++ src/main/java/com/ditto/cookiez/controller/UserController.java
@@ -7,14 +7,8 @@ import com.ditto.cookiez.utils.Response;
 import com.ditto.cookiez.utils.ResponseMsg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.websocket.server.PathParam;
-import java.util.Map;
 
 /**
  * <p>
@@ -80,35 +74,35 @@ public class UserController {
     IUserService service;
 
     @GetMapping("/login")
-    public ModelAndView  loginPage() {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("/login");
-        return mv;
+    public ModelAndView loginPage() {
+        return new ModelAndView("/user/login");
     }
 
     @PostMapping("/api/login")
     public ResponseEntity<JSONObject> login(@RequestBody JSONObject loginParams) {
         String username = loginParams.getString("username");
         String password = loginParams.getString("password");
-        service.auth(username,password);
-        User user=service.getByUsername(username);
-        if(user!=null){
-            return Response.ok(ResponseMsg.SUCCEED_TO_LOGIN.v(),user);
-        }else {
+        service.auth(username, password);
+        User user = service.getByUsername(username);
+        if (user != null) {
+            return Response.ok(ResponseMsg.SUCCEED_TO_LOGIN.v(), user);
+        } else {
             return Response.bad("Failed to login");
         }
     }
 
     @GetMapping("/register")
-    public String registerPage() {
-        return "register";
+    public ModelAndView registerPage() {
+        return new ModelAndView("user/register");
+
+
     }
 
     @PostMapping("/api/register")
-    public ResponseEntity<JSONObject> register(@RequestBody JSONObject param){
-        String username=param.getString("username"),password=param.getString("password");
-        User user=new User(username,password);
-        if( service.save(user)){
+    public ResponseEntity<JSONObject> register(@RequestBody JSONObject param) {
+        String username = param.getString("username"), password = param.getString("password");
+        User user = new User(username, password);
+        if (service.save(user)) {
             return Response.ok("Succeed to register");
         }
         return Response.bad("Failed to register");
