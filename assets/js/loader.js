c578c8a9e09303e8b73110f4759f3b72390adad7
Zhihao Liang <s3798366@student.rmit.edu.au>
2020-09-15 23:54:22 +0800

    add github table, team meeing and modified css js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,7 +1,12 @@
-$(document).ready(function () {
+$(document).ready(function () {    $("head").append(`
+
+
+`)
   try {
+
     $("#headerDiv").load("../../../template/header.html");
     $("#footerDiv").load("../../../template/footer.html");
+    $("a").attr("target", "_blank");
   } catch (error) {}
   // try {
   //   $("#headerDiv").load("./template/header.html");
