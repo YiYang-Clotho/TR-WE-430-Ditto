3544afd6d3693bc9de624f5ff92e20df3f2aa886
Zhihao Liang <s3798366@student.rmit.edu.au>
2020-09-29 19:54:26 +0800

    feat(All): user can upload a recipe

---
+++ src/main/java/com/ditto/cookiez/mapper/IngredientRecipeBridgeMapper.java
@@ -0,0 +1,16 @@
+package com.ditto.cookiez.mapper;
+
+import com.ditto.cookiez.entity.IngredientRecipeBridge;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author astupidcoder
+ * @since 2020-09-29
+ */
+public interface IngredientRecipeBridgeMapper extends BaseMapper<IngredientRecipeBridge> {
+
+}
---
+++ src/main/java/com/ditto/cookiez/service/IRecipeTagBridgeService.java
@@ -0,0 +1,16 @@
+package com.ditto.cookiez.service;
+
+import com.ditto.cookiez.entity.RecipeTagBridge;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author astupidcoder
+ * @since 2020-09-29
+ */
+public interface IRecipeTagBridgeService extends IService<RecipeTagBridge> {
+
+}
