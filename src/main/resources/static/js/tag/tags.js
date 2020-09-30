c04a20e02d2b9ef4922eba8951a5738b53c93722
Yi Yang <s3798354@student.rmit.edu.au>
2020-09-30 14:19:21 +1000

    justify like

---
+++ src/main/resources/static/js/tag/tags.js
@@ -0,0 +1,13 @@
+$('#btn').click(function(){
+    $(this).toggleClass('active');
+});
+
+// Filter table
+$(document).ready(function(){
+  $("#tableSearch").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
